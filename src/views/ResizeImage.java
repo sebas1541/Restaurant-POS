ad6c6ff64809cbb45a2e0ed366d50663757d3edc
sebas1541 <sebastian.ca0102@gmail.com>
2023-11-13 01:03:12 -0500

    Login View update, Resize Image class with method resize to instance image resizing, new package resources, for photos, and content

---
+++ src/views/ResizeImage.java
@@ -0,0 +1,14 @@
+package views;
+
+import javax.swing.*;
+import java.awt.*;
+
+public  class ResizeImage {
+
+    public ImageIcon resize(ImageIcon img, int width, int height ){
+        Image image = img.getImage();
+        Image resizedImage = image.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+        ImageIcon resizedIcon = new ImageIcon(resizedImage);
+        return resizedIcon;
+    }
+}
